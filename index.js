28f544b1464220ee26ab8f2937f952ecea285fc4
Jonathan <jonathanmoorevla@gmail.com>
2022-06-10 16:38:57 -0500

    commit tree 22ed560911b90d9d85f0115880ee7025b02cf201

---
+++ index.js
@@ -0,0 +1,14 @@
+let num1 = 2000;
+let num2 = 100;
+
+function divide(num1, num2) {
+    return num1/num2;
+};
+
+const square = (num3) => num3**2;
+
+
+let num4 = 3;
+let num5 = 4;
+
+const add = (num4, num5) => num4 + num5;
